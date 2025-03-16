ee3af6cb459cc505fda07ce5e86b8fad2a94e64a
TuanAnh <tuanmeo980provip@gmail.com>
2025-03-16 21:52:05 +0700

    edit Btn

--- src/customs/Button/ButtonDark.jsx
+++ src/customs/Button/ButtonDark.jsx
@@ -9,7 +9,7 @@ const ButtonDark = ({ darkMode, toggleDarkMode }) => {
                 border-gray-400 
                 ${darkMode ? "hover:bg-gray-200" : "hover:bg-gray-800"}`}
         >
-            {darkMode ? <Sun className="text-yellow-500" /> : <Moon className="text-gray-600" />}
+            {darkMode ? <Sun className="text-yellow-500" /> : <Moon className="text-gray-600 hover:text-white" />}
         </button>
     );
 };
