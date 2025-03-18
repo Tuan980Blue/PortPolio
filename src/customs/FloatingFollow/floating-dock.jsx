a161149766df6fd864f36fe2e7e6c5bba7eae30e
TuanAnh <tuanmeo980provip@gmail.com>
2025-03-18 20:53:01 +0700

    add infor abountme

--- src/customs/FloatingFollow/floating-dock.jsx
+++ src/customs/FloatingFollow/floating-dock.jsx
@@ -77,7 +77,7 @@ function IconContainer({ mouseX, title, icon, href }) {
                 style={{ width, height }}
                 onMouseEnter={() => setHovered(true)}
                 onMouseLeave={() => setHovered(false)}
-                className="aspect-square rounded-full bg-gray-200 dark:bg-neutral-800 flex items-center justify-center relative">
+                className="aspect-square rounded-full bg-gray-200 flex items-center justify-center relative">
                 <AnimatePresence>
                     {hovered && (
                         <motion.div
